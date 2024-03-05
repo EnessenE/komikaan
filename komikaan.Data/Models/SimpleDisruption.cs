e29fd82a33ab221fc7d68d817e0f5bd52373286a
Enes <9263360+EnessenE@users.noreply.github.com>
2024-03-05 12:05:12 +0100

    Bugfix/calamities (#12)
    * Make items nullable or attempt to retrieve other data
    
    * Update SimpleDisruption.cs
    
    * Pass url
    
    * Nullable & changelog

--- komikaan.Data/Models/SimpleDisruption.cs
+++ komikaan.Data/Models/SimpleDisruption.cs
@@ -9,13 +9,19 @@ public class SimpleDisruption
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public DataSource Source { get; set; }
     public string Title { get; set; }
-    public DateTime Start { get; set; }
+    public DateTime? Start { get; set; }
     public DateTime? ExpectedEnd { get; set; }
     public IEnumerable<string> Descriptions { get; set; }
+
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public DisruptionType Type { get; set; }
     public DisruptionStage Stage { get; set; }
-    public IEnumerable<string> Advices { get; set; }
+    public IEnumerable<string>? Advices { get; set; }
     public bool Active { get; set; }
     public List<string> AffectedStops { get; set; }
+
+    /// <summary>
+    /// URL with possibly more info
+    /// </summary>
+    public string? Url { get; set; }
 }
