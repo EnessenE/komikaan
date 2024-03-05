bbb6c0dd97e9c4c1e1b9dca20bcce1048c0dbfc0
Enes <9263360+EnessenE@users.noreply.github.com>
2024-03-05 11:52:14 +0100

    Update SimpleDisruption.cs

--- komikaan.Data/Models/SimpleDisruption.cs
+++ komikaan.Data/Models/SimpleDisruption.cs
@@ -12,10 +12,16 @@ public class SimpleDisruption
     public DateTime Start { get; set; }
     public DateTime? ExpectedEnd { get; set; }
     public IEnumerable<string> Descriptions { get; set; }
+
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public DisruptionType Type { get; set; }
     public DisruptionStage Stage { get; set; }
     public IEnumerable<string>? Advices { get; set; }
     public bool Active { get; set; }
     public List<string> AffectedStops { get; set; }
+
+    /// <summary>
+    /// URL with possibly more info
+    /// </summary>
+    public string? Url { get; set; }
 }
