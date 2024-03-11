61f6890c49b79f5a549fc7a5ce9e4eae9e69743c
Enes <9263360+EnessenE@users.noreply.github.com>
2024-03-11 11:48:01 +0100

    Update TripTimeInfo.cs

--- komikaan.Data/GTFS/TripTimeInfo.cs
+++ komikaan.Data/GTFS/TripTimeInfo.cs
@@ -1,4 +1,5 @@
-﻿using GTFS.Entities.Enumerations;
+﻿using System.Numerics;
+using GTFS.Entities.Enumerations;
 
 public class TripTimeInfo
 {
@@ -15,4 +16,19 @@ public class TripTimeInfo
     public RouteType? route_type { get; set; }
     public TimeSpan arrival_time_end { get; set; }
     public TimeSpan departure_time_start { get; set; }
+}
+
+public class RouteInfo
+{
+    public string out_stop_name { get; set; }
+    public string out_stop_id { get; set; }
+    public int out_stop_num { get; set; }
+    public BigInteger out_stop_pk { get; set; }
+    public double out_cost { get; set; }
+    public int seq { get; set; }
+    public int path_seq { get; set; }
+    public BigInteger start_vid { get; set; }
+    public BigInteger end_vid { get; set; }
+    public BigInteger node { get; set; }
+    public BigInteger edge { get; set; }
 }
