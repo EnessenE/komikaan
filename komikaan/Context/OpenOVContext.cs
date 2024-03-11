f91156b6c2f99c45596bddb9405384239d53c4b9
Enes <9263360+EnessenE@users.noreply.github.com>
2024-03-11 11:47:57 +0100

    Update OpenOVContext.cs

--- komikaan/Context/OpenOVContext.cs
+++ komikaan/Context/OpenOVContext.cs
@@ -134,36 +134,11 @@ namespace komikaan.Context
 
 
                 // Retrieve trip times between stops
-                var tripTimes = await dbConnection.QueryAsync<TripTimeInfo>(
-                    @"SELECT
-	                    1 as level,
-	                    startStopTimes.trip_id, 
-	                    trip.route_id, 
-	                    trip.trip_headsign, 
-	                    route.route_type, 
-	                    route.route_short_name, 
-	                    route.route_long_name, 
-	                    agency.agency_name,
-	                    startStopTimes.trip_id AS trip_id_start,
-	                    targetStop.platform_code AS platform_code_end,
-	                    startStop.stop_name AS stop_name_start,
-	                    startStopTimes.departure_time AS departure_time_start,
-	                    targetStopTimes.trip_id AS trip_id_end,
-	                    startStop.platform_code AS platform_code_start,
-	                    targetStop.stop_name AS stop_name_end,
-	                    targetStopTimes.arrival_time AS arrival_time_end
-                    FROM
-	                    stop_times startStopTimes
-                        JOIN trips trip ON startStopTimes.trip_id = trip.trip_id
-                        JOIN routes route ON trip.route_id = route.route_id
-                        JOIN agency agency ON agency.agency_id = route.agency_id
-                        JOIN stop_times targetStopTimes ON startStopTimes.trip_id = targetStopTimes.trip_id
-                        JOIN stops startStop ON startStopTimes.stop_id = startStop.stop_id
-                        JOIN stops targetStop ON targetStopTimes.stop_id = targetStop.stop_id
-                        JOIN calendar_dates dates ON trip.service_id = dates.service_id
-                    WHERE
-	                    startStopTimes.stop_id = ANY(@StartStopIds) AND targetStopTimes.stop_id = ANY(@EndStopIds) AND startStopTimes.departure_time < targetStopTimes.arrival_time
-                        and dates.date = @date",
+                var tripTimes = await dbConnection.QueryAsync<RouteInfo>(
+                @"SELECT out_stop_name, out_stop_id, out_stop_num, out_stop_pk, out_cost, seq, path_seq, start_vid, end_vid, node, edge from public.get_route_data(
+	                        @StartStopIds, 
+	                        @EndStopIds
+                        )",
                     new
                     {
                         StartStopIds = simpleFromStop.Ids[Supplier],
@@ -173,12 +148,23 @@ namespace komikaan.Context
                     commandType: CommandType.Text
                 );
 
-
+                
                 _logger.LogInformation("Found {routes} paths", tripTimes.Count());
                 var data = new List<SimpleTravelAdvice>();
+                var routes = new List<List<RouteInfo>>();
+                var last = int.MaxValue;
+                foreach (var info in tripTimes)
+                {
+                    if (info.path_seq < last)
+                    {
+                        //new one
+                        routes.Add(new List<RouteInfo>());
+                    }
+                    routes.Last().Add(info);
+                    last += 1;
+                }
 
-
-                foreach (var tripTime in tripTimes)
+                foreach (var route in routes)
                 {
                     var item = new SimpleTravelAdvice()
                     {
@@ -187,24 +173,9 @@ namespace komikaan.Context
                         PlannedDurationInMinutes = 999,
                         Route = new List<SimpleRoutePart>()
                     };
-                    var part = new SimpleRoutePart();
-                    part.LineName = tripTime.route_short_name;
-                    part.Direction = tripTime.trip_headsign;
-                    part.PlannedDepartureTrack = tripTime.platform_code_start?.ToString();
-                    part.PlannedArrivalTrack = tripTime.platform_code_end?.ToString();
-                    part.PlannedArrival = new DateTime(DateOnly.FromDateTime(DateTime.UtcNow),
-                        new TimeOnly(tripTime.arrival_time_end.Hours, tripTime.arrival_time_end.Minutes,
-                            tripTime.arrival_time_end.Seconds));
-                    part.PlannedDeparture = new DateTime(DateOnly.FromDateTime(DateTime.UtcNow),
-                        new TimeOnly(tripTime.departure_time_start.Hours, tripTime.departure_time_start.Minutes,
-                            tripTime.departure_time_start.Seconds));
-                    part.DepartureStation = tripTime.stop_name_start;
-                    part.ArrivalStation = tripTime.stop_name_end;
-                    part.Operator = tripTime.agency_name;
-                    part.RealisticTransfer = true;
-                    part.AlternativeTransport = false;
-                    part.Type = tripTime.route_type?.ToLegType() ?? LegType.Unknown;
-                    item.Route.Add(part);
+
+                    CalculateRoute(route, item);
+
 
                     var time = (item.Route.Last().PlannedArrival - item.Route.First().PlannedDeparture);
                     item.PlannedDurationInMinutes = time?.TotalMinutes ?? 0;
@@ -219,6 +190,51 @@ namespace komikaan.Context
                 _logger.LogInformation("A unrecognized stop was present {fromStop} - {toStop}", from, to);
                 return Enumerable.Empty<SimpleTravelAdvice>();
             }
+        }
+
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "AV1500:Member or local function contains too many statements", Justification = "TODO")]
+        private static void CalculateRoute(List<RouteInfo> route, SimpleTravelAdvice travelAdvice)
+        {
+            for (var routePartId = 0; routePartId < route.Count; routePartId++)
+            {
+                var routePart = route[routePartId];
+                var part = new SimpleRoutePart();
+
+                // part.LineName = tripTime.route_short_name;
+                // part.Direction = tripTime.trip_headsign;
+                // part.PlannedDepartureTrack = tripTime.platform_code_start?.ToString();
+                // part.PlannedArrivalTrack = tripTime.platform_code_end?.ToString();
+                // part.PlannedArrival = new DateTime(DateOnly.FromDateTime(DateTime.UtcNow),
+                //     new TimeOnly(tripTime.arrival_time_end.Hours, tripTime.arrival_time_end.Minutes,
+                //         tripTime.arrival_time_end.Seconds));
+                // part.PlannedDeparture = new DateTime(DateOnly.FromDateTime(DateTime.UtcNow),
+                //     new TimeOnly(tripTime.departure_time_start.Hours, tripTime.departure_time_start.Minutes,
+                //         tripTime.departure_time_start.Seconds));
+                // part.DepartureStation = tripTime.stop_name_start;
+                // part.ArrivalStation = tripTime.stop_name_end;
+                // part.Operator = tripTime.agency_name;
+                // part.RealisticTransfer = true;
+                // part.AlternativeTransport = false;
+                // part.Type = tripTime.route_type?.ToLegType() ?? LegType.Unknown;
+
+
+                part.PlannedArrival = DateTime.UtcNow;
+                part.PlannedDeparture = DateTime.UtcNow.AddMinutes(5);
+                part.DepartureStation = routePart.out_stop_name;
+                if (routePartId + 1 <= route.Count-1)
+                {
+                    part.ArrivalStation = route[routePartId + 1].out_stop_name;
+                }
+
+                part.Operator = "flixbus";
+                part.RealisticTransfer = true;
+                part.AlternativeTransport = false;
+                part.Type = LegType.Bus;
+
+                travelAdvice.Route.Add(part);
+            }
+            travelAdvice.Route.Remove(travelAdvice.Route.Last());
         }
     }
 }
