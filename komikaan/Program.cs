4f4968f292a135de1b82c21a6d460bb976beb5b8
Enes <9263360+EnessenE@users.noreply.github.com>
2024-03-19 13:31:55 +0100

    1.7.1 - Add OpenTelemetry support and refactor the controllers (#16)
    * Move logic to its own location
    
    * OpenTelemetry
    
    * Add activities
    
    * Rename NS_Api_Url
    
    * Bump version
    
    * Update tests

--- komikaan/Program.cs
+++ komikaan/Program.cs
@@ -6,12 +6,20 @@ using Refit;
 using Serilog;
 using System.Reflection;
 using komikaan.Data.Configuration;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics.Metrics;
+using System.Diagnostics;
 
 [System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "AV1500:Member or local function contains too many statements", Justification = "I dont like top levels, will receive a reformat ever.")]
 internal class Program
 {
     public static void Main(string[] args)
     {
+        var meter = new Meter("komikaan.api", "1.0.0");
+
+
         var builder = WebApplication.CreateBuilder(args);
 
         var corsName = "sources";
@@ -35,6 +43,8 @@ internal class Program
         builder.Services.AddSwaggerGen();
         builder.Services.Configure<SupplierConfigurations>(builder.Configuration.GetSection("SupplierMappings"));
 
+        CreateObservability(builder, builder.Services, meter);
+
         builder.Services.AddCors(options =>
         {
             options.AddPolicy(corsName,
@@ -57,7 +67,7 @@ internal class Program
         IHttpClientBuilder refitClientBuilder = builder.Services.AddRefitClient<INSApi>(refitSettings)
             .ConfigureHttpClient(httpClient =>
             {
-                httpClient.BaseAddress = new Uri(builder.Configuration.GetValue<string>("NSApiUrl")!);
+                httpClient.BaseAddress = new Uri(builder.Configuration.GetValue<string>("NS_Api_Url")!);
                 httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", builder.Configuration.GetValue<string>("ns_api_key"));
                 // Will throw `TaskCanceledException` if the request goes longer than 3 seconds.
                 httpClient.Timeout = TimeSpan.FromSeconds(10);
@@ -69,9 +79,52 @@ internal class Program
         AddDataSuppliers(builder);
         builder.Services.AddHostedService<DataService>();
         builder.Services.AddSingleton<IStopManagerService, StopManagerService>();
-
+        builder.Services.AddSingleton<ITravelAdviceHandler, TravelAdviceHandler>();
 
         SetupApplication(builder, corsName);
+    }
+
+    private static void CreateObservability(IHostApplicationBuilder builder, IServiceCollection services,
+        Meter meter)
+    {
+        var tracingOtlpEndpoint = builder.Configuration.GetValue<string>("OTLP_ENDPOINT_URL");
+
+        var greeterActivitySource = new ActivitySource(meter.Name);
+
+        var otel = services.AddOpenTelemetry();
+
+        // Configure OpenTelemetry Resources with the application name
+        otel.ConfigureResource(resource => resource
+            .AddService(builder.Environment.ApplicationName));
+
+        // Add Metrics for ASP.NET Core and our custom metrics and export to Prometheus
+        otel.WithMetrics(metrics => metrics
+            // Metrics provider from OpenTelemetry
+            .AddAspNetCoreInstrumentation()
+            .AddMeter(meter.Name)
+            // Metrics provides by ASP.NET Core in .NET 8
+            .AddMeter("Microsoft.AspNetCore.Hosting")
+            .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+            .AddPrometheusExporter());
+
+        // Add Tracing for ASP.NET Core and our custom ActivitySource and export to Jaeger
+        otel.WithTracing(tracing =>
+        {
+            tracing.AddAspNetCoreInstrumentation();
+            tracing.AddHttpClientInstrumentation();
+            tracing.AddSource(greeterActivitySource.Name);
+            if (tracingOtlpEndpoint != null)
+            {
+                tracing.AddOtlpExporter(otlpOptions =>
+                {
+                    otlpOptions.Endpoint = new Uri(tracingOtlpEndpoint);
+                });
+            }
+            else
+            {
+                tracing.AddConsoleExporter();
+            }
+        });
     }
 
     private static void AddDataSuppliers(WebApplicationBuilder builder)
@@ -89,6 +142,7 @@ internal class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+        app.MapPrometheusScrapingEndpoint();
 
         app.UseCors(corsName);
 
---
+++ komikaan/Telemetry.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace komikaan
+{
+    public static class Telemetry
+    {
+        /// <summary>
+        /// ActivitySource instance, only one should exist per application
+        /// </summary>
+        public static readonly ActivitySource Activity = new("komikaan.api");
+    }
+}
