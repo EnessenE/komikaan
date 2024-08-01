b1cc446afba9772f174c919052cefc984af6b099
Enes <9263360+EnessenE@users.noreply.github.com>
2024-08-01 22:32:16 +0200

    2.3.0 - Attempt to look at days, prevent some duplicates

--- komikaan.Data/GTFS/GTFSTripStop.cs
+++ komikaan.Data/GTFS/GTFSTripStop.cs
@@ -5,17 +5,19 @@
         public string Id { get; set; }
         public int Sequence { get; set; }
         public string Name { get; set; }
-        public TimeOnly? Arrival { get; set; }
-        public TimeOnly? Departure { get; set; }
+        public DateTimeOffset? Arrival { get; set; }
+        public DateTimeOffset? Departure { get; set; }
         public string PlatformCode { get; set; }
         public string StopHeadsign { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
         public StopType StopType { get; set; }
-        public TimeOnly? ActualArrivalTime { get => Arrival; }
-        public TimeOnly? ActualDepartureTime { get => Departure; }
-        public TimeOnly? PlannedArrivalTime { get => Arrival; }
-        public TimeOnly? PlannedDepartureTime { get => Departure; }
+        public int PickupType { get; set; }
+        public int DropOffType { get; set; }
+        public DateTimeOffset? ActualArrivalTime { get => Arrival; }
+        public DateTimeOffset? ActualDepartureTime { get => Departure; }
+        public DateTimeOffset? PlannedArrivalTime { get => Arrival; }
+        public DateTimeOffset? PlannedDepartureTime { get => Departure; }
         public string PlannedPlatform { get => PlatformCode; }
         public string ActualPlatform { get => PlatformCode; }
     }
