2aae4eb3f9662947f4c703b86ee3d3ad90931629
Enes <9263360+EnessenE@users.noreply.github.com>
2024-08-18 23:21:10 +0200

    2.6.0

--- komikaan.Data/GTFS/GTFSRoute.cs
+++ komikaan.Data/GTFS/GTFSRoute.cs
@@ -4,5 +4,13 @@
     {
         public string ShortName { get; set; }
         public string LongName { get; set; }
+        public string DataOrigin { get; set; }
+        public string Agency { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string Url { get; set; }
+        public string Color { get; set; }
+        public string TextColor { get; set; }
+        public DateTime LastUpdated { get; set; }
     }
 }
