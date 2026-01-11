3268fc2a6e0fc0acefa369972955026e61ae6ed2
Enes <9263360+EnessenE@users.noreply.github.com>
2026-01-11 14:56:01 +0100

    2.17.0 - Coverage

---
+++ komikaan.Data/Models/CoverageDataPoint.cs
@@ -0,0 +1,12 @@
+﻿using komikaan.GTFS.Models.Static.Enums;
+
+namespace komikaan.Data.Models;
+
+public class CoverageDataPoint
+{
+    public string DataOrigin { get; set; }
+    public double Longitude { get; set; }
+    public double Latitude { get; set; }
+    public int ClusterId { get; set; }
+    public ExtendedRouteType StopType { get; set; }
+}
