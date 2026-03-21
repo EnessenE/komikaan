ee7301f2a02b5535868e61edec4e0d8eee80f9de
Enes <9263360+EnessenE@users.noreply.github.com>
2026-03-21 12:14:58 +0100

    Route details

---
+++ komikaan.Data/GTFS/GTFSRouteServiceDateRange.cs
@@ -0,0 +1,8 @@
+namespace komikaan.Data.GTFS
+{
+    public class GTFSRouteServiceDateRange
+    {
+        public DateOnly? FirstRun { get; set; }
+        public DateOnly? LastRun { get; set; }
+    }
+}
